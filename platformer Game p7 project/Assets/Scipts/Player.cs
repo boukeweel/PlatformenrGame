5404e85b54b7904541036ce263991751b0d3165b
Bouke <BoukeWeel@outlook.com>
2020-02-14 16:14:24 +0100

    sliding en double jump

--- platformer Game p7 project/Assets/Scipts/Player.cs
+++ platformer Game p7 project/Assets/Scipts/Player.cs
@@ -18,7 +18,7 @@ public class Player : MonoBehaviour
     [Header("Jumping Varibelen")]
     //everything for jumping
     public float MaxJumping = 2;
-    float jumps;
+    float jumps = 0;
     public float Jumpingforce = 2f;
     bool grounded = false;
     public float MaxJumpingHeigt;
@@ -38,11 +38,23 @@ public class Player : MonoBehaviour
     Vector3 movement;
     //public Jumpingstate M_jumpingstate = Jumpingstate.Grounded;
 
+    SpriteRenderer srend;
+
+    //for sliddig
+    public float slidingspeed;
+    public BoxCollider2D normaleCol;
+    public BoxCollider2D SlidingCol;
+    public bool sliding;
+
+    public bool allowedTomove;
+
+
     private void Start()
     {
         rig2d = GetComponent<Rigidbody2D>();
         S_Speed = speed;
         S_Jumpingforce = Jumpingforce;
+        srend = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
@@ -50,7 +62,7 @@ public class Player : MonoBehaviour
         Xaxis = XCI.GetAxis(XboxAxis.LeftStickX, PlayerNumber);
         //Yaxis = XCI.GetAxis(XboxAxis.LeftStickY, PlayerNumber);
 
-
+        //sprinting
         if (XCI.GetButtonDown(XboxButton.LeftBumper, PlayerNumber))
         {
             speed *= 1.5f;
@@ -60,7 +72,7 @@ public class Player : MonoBehaviour
             speed /= 1.5f;
         }
 
-        
+        //movement
         if(grounded == true)
         {
             transform.Translate(new Vector3(Xaxis * Time.deltaTime * speed, 0f, 0f));
@@ -70,11 +82,19 @@ public class Player : MonoBehaviour
             //On Jumping not ably to move
             transform.Translate(new Vector3(S_Xasis * Time.deltaTime * S_Speed, 0f, 0f));
         }
+        if(Xaxis > 0.1f)
+        {
+            srend.flipX = false;
+        }
+        if(Xaxis < -0.1f)
+        {
+            srend.flipX = true;
+        }
             
         
         
         //jump heiger if you hold 
-        if (XCI.GetButton(XboxButton.A, PlayerNumber))
+        if (XCI.GetButton(XboxButton.A, PlayerNumber) && grounded == true)
         {
             Jumpingforce += 0.1f;
             if(Jumpingforce > MaxJumpingHeigt)
@@ -82,7 +102,6 @@ public class Player : MonoBehaviour
                 Jumpingforce = MaxJumpingHeigt + 1;
             }
         }
-
         //jump
         if (XCI.GetButtonUp(XboxButton.A, PlayerNumber) && grounded == true)
         {
@@ -90,17 +109,55 @@ public class Player : MonoBehaviour
             S_Speed = speed;
             rig2d.velocity = Vector2.up * Jumpingforce;
             Jumpingforce = S_Jumpingforce;
+            jumps++;
             grounded = false;
         }
-        
+        //double jump
+        if(XCI.GetButtonDown(XboxButton.A, PlayerNumber) && jumps < MaxJumping && grounded == false)
+        {
+            rig2d.velocity = Vector2.up * 10;
+            jumps++;
+        }
 
+        if(XCI.GetButtonDown(XboxButton.B,PlayerNumber) && grounded == true)
+        {
+            Slidding();
+        }
+        if (srend.flipX == false && sliding == true)
+        {
+            rig2d.AddForce(Vector2.right * slidingspeed);
+        }
+        if (srend.flipX == true && sliding == true )
+        {
+            rig2d.AddForce(Vector2.left * slidingspeed);
+        }
 
     }
+    public void Slidding()
+    {
+        sliding = true;
+
+        //normaleCol.enabled = false;
+        //SlidingCol.enabled = true;
+
+        
+        StartCoroutine("stopSliding");
+
+    }
+    IEnumerator stopSliding()
+    {
+        yield return new WaitForSeconds(0.8f);
+        sliding = false;
+
+    }
+
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("Ground"))
         {
             grounded = true;
+            jumps = 0;
         }
     }
 
